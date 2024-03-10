5da5e9f1a5ed0aa2a69f13b66d8d5d318c7699c3
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-03-10 04:07:13 +0100

    Initial commit

---
+++ src/main/java/com/naru/shareathought/ShareAThoughtApplication.java
@@ -0,0 +1,13 @@
+package com.naru.shareathought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareAThoughtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareAThoughtApplication.class, args);
+	}
+
+}
